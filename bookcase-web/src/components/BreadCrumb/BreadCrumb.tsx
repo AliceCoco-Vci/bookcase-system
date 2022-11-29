f98ec63ee6cb01ff82eeebf119b0088a4a77d6f2
Dihan Li <lidihan@hyperchain.cn>
2022-11-29 15:25:37 +0800

    ts

--- bookcase-web/src/components/BreadCrumb/BreadCrumb.tsx
+++ bookcase-web/src/components/BreadCrumb/BreadCrumb.tsx
@@ -2,7 +2,7 @@
  * @Author: Dihan Li lidihan@hyperchain.cn
  * @Date: 2022-10-18 09:46:33
  * @LastEditors: Dihan Li lidihan@hyperchain.cn
- * @LastEditTime: 2022-11-01 14:05:56
+ * @LastEditTime: 2022-11-29 15:17:05
  * @FilePath: /bookcase-web/src/components/BreadCrumb/BreadCrumb.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,7 +21,7 @@ interface BreadCrumbProps {
 const BreadCrumb = ({ layer }: BreadCrumbProps) => {
     return (
         <Breadcrumb separator={<LineOutlined style={{ width: 100, color: '#7eb79d' }} />}>
-            {layer?.map((data: any) => (
+            {layer?.map((data) => (
                 <Breadcrumb.Item
                     key={data.label as string}>
                     <Link to={data.url}>{data.label}</Link>
